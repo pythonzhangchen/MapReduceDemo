451ccb7f88ebf3b78e1d3deecec1d68fead4927f
-_- <429478948@qq.com>
2022-08-24 22:49:59 +0800

    combineTextinputFormat

---
+++ src/main/java/com/zhang/mapreduce/combineTextInputFormat/WordCountReducer.java
@@ -0,0 +1,32 @@
+package com.zhang.mapreduce.combineTextInputFormat;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+/**
+ * KEYIN, map阶段输入key的类型：Text
+ * VALUEIN, map阶段输入value类型：IntWritable
+ * KEYOUT, map阶段输出的key类型：Text
+ * VALUEOUT, map阶段输出的value类型：IntWritable
+ */
+public class WordCountReducer extends Reducer<Text,IntWritable,Text, IntWritable> {
+    private  IntWritable outV = new IntWritable();
+
+    @Override
+    protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+        int sum=0;
+
+        // 累加
+        for (IntWritable value : values) {
+            sum += value.get();
+        }
+
+        outV.set(sum);
+        // 写出
+        context.write(key,outV);
+    }
+
+}
